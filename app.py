73429d417796122b1c71ad800b08e6426d55bcdd
Sail <gangan.sail@gmail.com>
2022-07-23 19:46:47 +0530

    Fixing init_app issue

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
+from db import db, create_app
 
-app = Flask(__name__)
-db.init_app(app)
+app = create_app()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Sail'
